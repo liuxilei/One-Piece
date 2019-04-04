43679209d193985604fa95de5da39465b02e0610
liuxilei <liuxileioo@qq.com>
2019-04-04 15:24:40 +0800

    :feat redux练习

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
+import Counter from './Demo/Counter';
+import counter from './Demo/Counter/reducer';
+import { createStore } from 'redux';
 
-class Demo extends Component {
-    render() {
-        return (
-            <div>Hello world</div>
-        )
-    }
-};
+const store = createStore(counter);
 
-ReactDOM.render(
-    <Demo />,
+const render = () => ReactDOM.render(
+    <Counter 
+        score={store.getState()}
+        onIncrement={() => store.dispatch({type: 'INCREMENT'})}
+        onDecrement={() => store.dispatch({type: 'DECREMENT'})}
+        />,
     document.getElementById('root')
-)
+);
+
+render();
+store.subscribe(render);
