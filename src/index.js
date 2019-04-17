f8a2caba546e5416e19289a4c23c2f502837cd15
liuxilei <liuxileioo@qq.com>
2019-04-17 12:36:54 +0800

    :feat 添加generator支持

--- src/index.js
+++ src/index.js
@@ -1,21 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-import App from './Demo/Todo/components/App';
-import RootReducer from './Demo/Todo/reducers';
-import './common/font/iconfont.css';
-
-const store = createStore(
-    RootReducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
-const rootEl = document.getElementById('root');
-
-
-ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
-    rootEl
-);
+function* testGenerator() {
+    yield '1';
+    yield '2';
+    return '3';
+}
+let g = testGenerator();
+console.log(g.next());
+console.log(g.next());
+console.log(g.next());
+console.log(g.next());
---
+++ src/index1.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import App from './Demo/Todo/components/App';
+import Test from './Demo/Test';
+import RootReducer from './Demo/Todo/reducers';
+import './common/font/iconfont.css';
+
+const store = createStore(
+    RootReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+const rootEl = document.getElementById('root');
+
+
+ReactDOM.render(
+    <Provider store={store}>
+        <Test />
+    </Provider>,
+    rootEl
+);
