6e73b0202c0ab1a80ea563c32d713dbba19df080
liuxilei <liuxileioo@qq.com>
2019-05-05 22:14:52 +0800

    :feat warining

--- src/index.js
+++ src/index.js
@@ -1,21 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-import App from './Demo/Todo/components/App';
-import RootReducer from './Demo/Todo/reducers';
-import './common/font/iconfont.css';
-
-const store = createStore(
-    RootReducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
-const rootEl = document.getElementById('root');
-
-
-ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
-    rootEl
-);
+class Person {
+    constructor(name = 'onePiece') {
+        this.name = name;
+    }
+}
+let lxl = new Person('liuxilei');
+console.log('实例',lxl);
