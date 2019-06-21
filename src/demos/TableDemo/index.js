b15eece1f791b3caee75905c89f95419f709d586
liuxilei <liuxileioo@qq.com>
2019-06-21 11:27:57 +0800

    :feat table无数据时候,内容无滚动条问题，查询官方issue也未解决,后续关注

--- src/demos/TableDemo/index.js
+++ src/demos/TableDemo/index.js
@@ -1,6 +1,22 @@
 import React, { Component } from 'react';
+import { Resizable } from 'react-resizable';
 import { Table } from 'antd';
+import "./index.scss";
 
+
+const ResizeableTitle = props => {
+    const { onResize, width, ...restProps } = props;
+
+    if (!width) {
+        return <th {...restProps} />;
+    }
+
+    return (
+        <Resizable width={width} height={0} onResize={onResize}>
+            <th {...restProps} />
+        </Resizable>
+    );
+};
 class TableDemo extends Component {
     state = {
         columns: [
@@ -8,13 +24,13 @@ class TableDemo extends Component {
                 title: 'Date',
                 dataIndex: 'date',
                 width: 200,
-                fixed: 'left',
+                //fixed: 'left',
             },
             {
                 title: 'Amount',
                 dataIndex: 'amount',
                 width: 100,
-                fixed: 'left',
+                //fixed: 'left',
             },
             {
                 title: 'Type',
@@ -30,7 +46,7 @@ class TableDemo extends Component {
                 title: 'Note',
                 dataIndex: 'note',
                 width: 100,
-                fixed: 'right',
+                //fixed: 'right',
                 render: (text, record) => (
                     <div style={{ wordWrap: 'break-word', wordBreak: 'break-all' }}>
                         {text}
@@ -40,43 +56,69 @@ class TableDemo extends Component {
             {
                 title: 'Action',
                 key: 'action',
-                fixed: 'right',
+                //fixed: 'right',
                 render: () => <a href="javascript:;">Delete</a>,
             },
         ],
     }
 
+    components = {
+        header: {
+            cell: ResizeableTitle,
+        },
+    };
+
     data = [
-        {
-            key: 0,
-            date: '2018-02-11',
-            amount: 120,
-            type: 'incomeaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
-            note: 'transfer',
-        },
-        {
-            key: 1,
-            date: '2018-03-11',
-            amount: 243,
-            type: 'incomevvvv',
-            note: 'transferbbbbbbbbbbbbbbbccccbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb',
-        },
-        {
-            key: 2,
-            date: '2018-04-11dddddddddddd',
-            amount: 98,
-            type: 'incometttttttttttttttttttttttttttttttttttttttttttttttttttttt',
-            note: 'transferccccccccccccccccccccccccccccccccccccc',
-        },
+        // {
+        //     key: 0,
+        //     date: '2018-03-11',
+        //     amount: 256,
+        //     type: 'afadada',
+        //     note: 'ygfbbbbbbbbbdfggddddddddddddddddddddddddd',
+        // },
+        // {
+        //     key: 1,
+        //     date: '2018-03-11',
+        //     amount: 243,
+        //     type: 'incomevvvv',
+        //     note: 'transferbbbbbbbbbbbbbbbccccbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb',
+        // },
+        // {
+        //     key: 2,
+        //     date: '2018-04-11dddddddddddd',
+        //     amount: 98,
+        //     type: 'incometttttttttttttttttttttttttttttttttttttttttttttttttttttt',
+        //     note: 'transferccccccccccccccccccccccccccccccccccccc',
+        // },
     ];
 
+    handleResize = index => (e, { size }) => {
+        this.setState(({ columns }) => {
+            const nextColumns = [...columns];
+            nextColumns[index] = {
+                ...nextColumns[index],
+                width: size.width,
+            };
+            return { columns: nextColumns };
+        });
+    };
+
     render() {
+        const columns = this.state.columns.map((col, index) => ({
+            ...col,
+            onHeaderCell: column => ({
+                width: column.width,
+                onResize: this.handleResize(index),
+            }),
+        }));
         return (
             <Table
                 bordered
-                style={{ width: "1200px" }}
-                scroll={{ x: 800, y: 300 }}
-                columns={this.state.columns}
+                components={this.components}
+                columns={columns}
+                style={{ width: "300px" }}
+                scroll={{ x: 300, y: 300 }}
+                //columns={this.state.columns}
                 dataSource={this.data}
                 pagination={false}
             />
