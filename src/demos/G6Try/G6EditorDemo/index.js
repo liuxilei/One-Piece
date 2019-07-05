c304e15e23e298d7b618af1dd7466a5fe6e8135d
liuxilei <liuxileioo@qq.com>
2019-07-05 17:50:29 +0800

    :feat g6editor 尝试

--- src/demos/G6Try/G6EditorDemo/index.js
+++ src/demos/G6Try/G6EditorDemo/index.js
@@ -1,5 +1,6 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component, createRef } from 'react';
 import G6Editor from '@antv/g6-editor';
+import "./index.scss";
 
 class G6EditorDemo extends Component {
     constructor(props) {
@@ -11,28 +12,35 @@ class G6EditorDemo extends Component {
         this.itempannel = null;
         this.detailpannel = null;
         this.page = null;
+        //创建DOM
+        this.minimap = createRef();
+        this.toolbar = createRef();
+        this.itempannel = createRef();
+        this.detailpannel = createRef();
+        this.contextmenu = createRef();
+        this.page = createRef();
     }
 
     componentDidMount() {
         this.editor = new G6Editor();
         this.minimap = new G6Editor.Minimap({
-            container: 'minimap'
+            container: this.minimap.current
         });
         this.toolbar = new G6Editor.Toolbar({
-            container: 'toobar'
+            container: this.toolbar.current
         });
         this.contextmenu = new G6Editor.Contextmenu({
-            container: 'contextmenu'
+            container: this.contextmenu.current
         });
         this.itempannel = new G6Editor.Itempannel({
-            container: 'itempannel'
+            container: this.itempannel.current
         });
         this.detailpannel = new G6Editor.Detailpannel({
-            container: 'detailpannel'
+            container: this.detailpannel.current
         });
         this.page = new G6Editor.Flow({
             graph: {
-                container: 'page'
+                container: this.page.current
             }
         });
         this.editor.add(this.minimap);
@@ -45,14 +53,14 @@ class G6EditorDemo extends Component {
 
     render() {
         return (
-            <Fragment>
-                <div id="minimap"></div>
-                <div id="toolbar"></div>
-                <div id="itempannel"></div>
-                <div id="detailpannel"></div>
-                <div id="contextmenu"></div>
-                <div id="page"></div>
-            </Fragment>
+            <div className="main">
+                <div id="minimap" ref={this.minimap}></div>
+                <div id="toolbar" ref={this.toolbar}></div>
+                <div id="itempannel" ref={this.itempannel}></div>
+                <div id="detailpannel" ref={this.detailpannel}></div>
+                <div id="contextmenu" ref={this.contextmenu}></div>
+                <div id="page" ref={this.page}></div>
+            </div>
         )
     }
 }
