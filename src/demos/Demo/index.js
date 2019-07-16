139cc7796f9d2f908a798d6c5b9ac85a45c3ef28
liuxilei <liuxileioo@qq.com>
2019-07-16 09:44:36 +0800

    :feat react-helmet组件试验

--- src/demos/Demo/index.js
+++ src/demos/Demo/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Helmet } from "react-helmet";
 
 class Demo extends Component {
     constructor(props) {
@@ -11,6 +12,11 @@ class Demo extends Component {
     render() {
         return (
             <div>
+                <Helmet>
+                    <meta charSet="utf-8" />
+                    <title>这是更改后的标题</title>
+                    <link rel="canonical" href="http://mysite.com/example" />
+                </Helmet>
                 {
                     this.state.value.map(item => {
                         return <div>{item.name}</div>
