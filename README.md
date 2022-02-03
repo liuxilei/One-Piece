23f95885c709665879ab0ad57fa1b39be75c665e
liuxilei <liuxileioo@qq.com>
2022-02-03 09:59:25 +0800

    ci: commitlint优化

--- README.md
+++ README.md
@@ -1,30 +1,9 @@
-## DEMOS
+## React study demo
 
-> 实践用 demo
+## CI
 
-基于 webpack 搭建 react 开发环境，后续可能单独弄成`cli`
-
-## Preview
-
-在线预览：https://liuxilei.github.io/One-Piece/#/
-
-源代码： https://github.com/liuxilei/One-Piece
-
-## Technology
-
-主要用到的技术：
-
--   React：MVVM 框架，用于构建前端界面。
--   Ant Design：基于 React 的组件库。
--   echarts: React 的图表库。
--   Less：CSS 预处理器，提供变量、计算、嵌套、Mixin、函数等。
--   sass: CSS 预处理器
--   styled-components: css in js
--   Webpack：打包前端项目，生成静态文件。
--   Jest：测试前后端项目，单元测试，API 测试等。
--   Typescript：为 JS 提供良好的类型检查功能，能编译出高质量的 JS 代码。
--   G6: 图可视化引擎
--   handsontable: 电子表格功能和外观的数据网格
+-   [prettier](https://prettier.io/)
+-   [commitlint](https://conventional-changelog.github.io/commitlint/)
 
 ## Start
 
--- src/index.tsx
+++ src/index.tsx
@@ -7,59 +7,53 @@ import zhCN from "antd/lib/locale-provider/zh_CN";
 import ErrorBoundary from "@/components/ErrorBoundary";
 import store from "./store";
 import history from "./utils/history";
-import { Switch, Route, Redirect, Router } from 'react-router-dom';
-import Login from './pages/Auth/Login';
+import { Switch, Route, Redirect, Router } from "react-router-dom";
+import Login from "./pages/Auth/login";
 import Loading from "./components/Loading/index";
 import "@/assert/font/iconfont.css";
 import "normalize.css";
 import "@/assert/global.css";
 
-
-const Layouts = lazy(() => import('./pages/Layouts/Layouts'));
+const Layouts = lazy(() => import("./pages/Layouts/Layouts"));
 
 const App = () => (
-    <StrictMode>
-        <ErrorBoundary>
-            <Provider store={store}>
-                <ConfigProvider locale={zhCN}>
-                    <Router history={history}>
-                        <Switch>
-                            <Route path="/login">
-                                <Login />
-                            </Route>
-                            <Route
-                                path="/"
-                                render={({ location }) =>
-                                    window.localStorage.getItem('token') ? (
-                                        <Suspense fallback={Loading}>
-                                            <Root />
-                                            {/* <Layouts /> */}
-                                        </Suspense>
-
-                                    ) : (
-                                        <Redirect
-                                            to={{
-                                                pathname: '/login',
-                                                state: { from: location },
-                                            }}
-                                        />
-                                    )
-                                }
-                            />
-                        </Switch>
-                    </Router>
-
-                </ConfigProvider>
-            </Provider>
-        </ErrorBoundary>
-    </StrictMode>
+	<StrictMode>
+		<ErrorBoundary>
+			<Provider store={store}>
+				<ConfigProvider locale={zhCN}>
+					<Router history={history}>
+						<Switch>
+							<Route path="/login">
+								<Login />
+							</Route>
+							<Route
+								path="/"
+								render={({ location }) =>
+									window.localStorage.getItem("token") ? (
+										<Suspense fallback={Loading}>
+											<Root />
+											{/* <Layouts /> */}
+										</Suspense>
+									) : (
+										<Redirect
+											to={{
+												pathname: "/login",
+												state: { from: location },
+											}}
+										/>
+									)
+								}
+							/>
+						</Switch>
+					</Router>
+				</ConfigProvider>
+			</Provider>
+		</ErrorBoundary>
+	</StrictMode>
 );
 
 if (module && module.hot) {
-    module.hot.accept()
+	module.hot.accept();
 }
 
-ReactDOM.render(
-    <App />,
-    document.getElementById("app")
-);
+ReactDOM.render(<App />, document.getElementById("app"));
